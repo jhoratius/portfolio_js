c0b313e13ffb61549ceb712790611f15ad3f6f9d
jhoratius <julien.horatius@gmail.com>
2024-07-02 23:12:43 +0200

    added a cube and a text

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -6,3 +7,25 @@ const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    renderer.render(scene, camera);
+    //cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+}
+renderer.setAnimationLoop(animate);
+
+// display a msg when WebGL is not supported
+if ( WebGL.isWebGL2Available() ) {
+	animate();
+} else {
+	const warning = WebGL.getWebGL2ErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+}
